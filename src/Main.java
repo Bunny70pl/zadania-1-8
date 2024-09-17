2bbb90d9be3d2f9a608e692ff694f7d9afd220d6
Bunny70pl <bunny70pl@gmail.com>
2024-09-17 21:12:11 +0200

    zad 4

--- src/Main.java
+++ src/Main.java
@@ -4,47 +4,47 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       for (int i = 10; i <100 ; i++) {
-           if(i % 2 == 0){
-               System.out.println(i);
-           }
-       }
-       System.out.println("Podaj slowo do sprawdzenia czy jest palindromem");
-       String slowo = wpiszStringZKlawiatury();
-       boolean palindrom = palindrom(slowo);
-       if(palindrom){
-           System.out.println("tak jest palindronem");
-       }else{
-           System.out.println("Nie jest palindronem");
-       }
-       System.out.println("Podaj liczbe do ktorej zostanie podana najblizsza liczba palindromowa");
-       Integer liczba = wpiszLiczbeZKlawiatury();
-       Integer liczbaNajblizszaPalindroma = najblizszaDoPalindroma(liczba);
-       System.out.println(liczbaNajblizszaPalindroma);
-       System.out.println("Podaj slowo do zaszyfrowania szyfrem cezara");
-       slowo = wpiszStringZKlawiatury();
-       String cezar = Cezar(slowo,3);
-       System.out.println(cezar);
-       System.out.println("Wpisz slowo");
-       ArrayList listaSlowo1 = new ArrayList<Character>();;
-       listaSlowo1 = wpiszDoTablicy();
-       System.out.println("Wpisz drugie slowo");
-       ArrayList listaSlowo2 = new ArrayList<Character>();
-       listaSlowo2 = wpiszDoTablicy();
-       System.out.println(czyAnagram(listaSlowo1,listaSlowo2));
-       System.out.println("Podaj liczbe do silni (n!)");
-       liczba = wpiszLiczbeZKlawiatury();
-       System.out.println("Silnia wynosi: " + silniaZPodanejLiczby(liczba));
-       System.out.println("Podaj liczbe do sprwadzenia czy jest liczba pierwsza");
-       liczba = wpiszLiczbeZKlawiatury();
-       System.out.println(czyLiczbaPierwsza(liczba));
-       System.out.println("Twoje haslo: "+ generujHaslo());
-       System.out.println("Tablica z pierwiastakami indeksow" +tablicaZPierwiastakimiZindeksow());
-       System.out.println("Tablica z ciagiem fiobnaciego" + tablicaZCiagaiemFibonaciego());
-       ArrayList<Integer> TablicaZCiagiemFibo = tablicaZCiagaiemFibonaciego();
-       System.out.println("Podaj liczbe do wyszukania: ");
-       liczba = wpiszLiczbeZKlawiatury();
-       System.out.println(znajdzNajblizszaLiczbe(TablicaZCiagiemFibo,liczba));
+       //for (int i = 10; i <100 ; i++) {
+       //    if(i % 2 == 0){
+       //        System.out.println(i);
+       //    }
+       //}
+       //System.out.println("Podaj slowo do sprawdzenia czy jest palindromem");
+       //String slowo = wpiszStringZKlawiatury();
+       //boolean palindrom = palindrom(slowo);
+       //if(palindrom){
+       //    System.out.println("tak jest palindronem");
+       //}else{
+       //    System.out.println("Nie jest palindronem");
+       //}
+       //System.out.println("Podaj liczbe do ktorej zostanie podana najblizsza liczba palindromowa");
+       //Integer liczba = wpiszLiczbeZKlawiatury();
+       //Integer liczbaNajblizszaPalindroma = najblizszaDoPalindroma(liczba);
+       //System.out.println(liczbaNajblizszaPalindroma);
+       //System.out.println("Podaj slowo do zaszyfrowania szyfrem cezara");
+       //slowo = wpiszStringZKlawiatury();
+       //String cezar = Cezar(slowo,3);
+       //System.out.println(cezar);
+       //System.out.println("Wpisz slowo");
+       //ArrayList listaSlowo1 = new ArrayList<Character>();;
+       //listaSlowo1 = wpiszDoTablicy();
+       //System.out.println("Wpisz drugie slowo");
+       //ArrayList listaSlowo2 = new ArrayList<Character>();
+       //listaSlowo2 = wpiszDoTablicy();
+       //System.out.println(czyAnagram(listaSlowo1,listaSlowo2));
+       //System.out.println("Podaj liczbe do silni (n!)");
+       //liczba = wpiszLiczbeZKlawiatury();
+       //System.out.println("Silnia wynosi: " + silniaZPodanejLiczby(liczba));
+       //System.out.println("Podaj liczbe do sprwadzenia czy jest liczba pierwsza");
+       //liczba = wpiszLiczbeZKlawiatury();
+       //System.out.println(czyLiczbaPierwsza(liczba));
+       //System.out.println("Twoje haslo: "+ generujHaslo());
+       //System.out.println("Tablica z pierwiastakami indeksow" +tablicaZPierwiastakimiZindeksow());
+       //System.out.println("Tablica z ciagiem fiobnaciego" + tablicaZCiagaiemFibonaciego());
+       //ArrayList<Integer> TablicaZCiagiemFibo = tablicaZCiagaiemFibonaciego();
+       //System.out.println("Podaj liczbe do wyszukania: ");
+       //liczba = wpiszLiczbeZKlawiatury();
+       //System.out.println(znajdzNajblizszaLiczbe(TablicaZCiagiemFibo,liczba));
 
         ArrayList<Integer> wylsowane = new ArrayList<>();
         ArrayList<Integer> parzyste = new ArrayList<>();
@@ -55,6 +55,12 @@ public class Main {
         System.out.println("Parzyste: " + parzyste);
         System.out.println("Nie parzyste: " + nieParzyste);
         System.out.println("Unikatowe: " + unikatowe);
+
+        Collections.sort(wylsowane);
+        System.out.println(wylsowane);
+        System.out.println("Mediana: " + mediana(wylsowane));
+        System.out.println("Dominanta: " + dominanta(wylsowane));
+        System.out.println("Srednia: " + srednia(wylsowane));
     }
     private static String wpiszStringZKlawiatury(){
         Scanner Klawiatura = new Scanner(System.in);
@@ -220,17 +226,56 @@ public class Main {
     }
     private static void wylosujLiczby(ArrayList<Integer> wylsowane,ArrayList<Integer> parzyste,ArrayList<Integer> nieParzyste,HashSet<Integer> unikatowe ){
 
-        while(wylsowane.size()<100){
+        while(wylsowane.size()<10){
             int liczba = (int)(Math.random()*100+1);
             wylsowane.add(liczba);
         }
         for (int i = 0; i < wylsowane.size(); i++) {
-            if(i % 2 == 0){
+            if(wylsowane.get(i) % 2 == 0){
                 parzyste.add(wylsowane.get(i));
             }else{
                 nieParzyste.add(wylsowane.get(i));
             }
             unikatowe.add(wylsowane.get(i));
         }
+    }
+    private static Double srednia(ArrayList<Integer> wylsowane){
+         Collections.sort(wylsowane);
+        double srednia = 0;
+        for (int i = 0; i < wylsowane.size(); i++) {
+            srednia+=wylsowane.get(i);
+        }
+        srednia = srednia/wylsowane.size();
+        return srednia;
+    }
+    private static double mediana(ArrayList<Integer> wylsowane){
+        Collections.sort(wylsowane);
+        double mediana = 0;
+        if(wylsowane.size()%2 == 0){
+            double liczba1 = wylsowane.get(wylsowane.size()/2-1);
+            double liczba2 = wylsowane.get(wylsowane.size()/2);
+            mediana = (liczba2 + liczba1)/2;
+        }else{
+            mediana = wylsowane.get(wylsowane.size()/2);
+        }
+        return mediana;
+    }
+    private static Integer dominanta(ArrayList<Integer> wylsowane){
+        Collections.sort(wylsowane);
+        int dominanta = wylsowane.get(0);
+        int maksymalnaLiczba = 0;
+        for (int i = 0; i < wylsowane.size(); i++) {
+            int obecnaLiczba = 0;
+            for (int j = 0; j < wylsowane.size(); j++) {
+                if (wylsowane.get(j) == wylsowane.get(i)) {
+                    obecnaLiczba++;
+                }
+            }
+            if (obecnaLiczba > maksymalnaLiczba) {
+                maksymalnaLiczba = obecnaLiczba;
+                dominanta = wylsowane.get(i);
+            }
+        }
+        return dominanta;
     }
 }
